0fe3b10eb702b134279a97bca0d88ba10dbf76ad
Lincoln Chawora <l.chawora@ctidigital.com>
2021-04-20 22:09:58 +0100

    Configure project to mostly use npm scripts. (PURGECSS not working yet)

--- postcss.config.js
+++ postcss.config.js
@@ -1,3 +1,32 @@
+const purgecss = require('@fullhuman/postcss-purgecss')
+const cssnano = require('cssnano')
+
+// Custom extractor for purgeCSS to avoid stripping classes with `:` prefixes.
+class TailwindExtractor {
+  static extract(content) {
+    // eslint-disable-next-line no-useless-escape
+    return content.match(/[A-z0-9-:\/]+/g) || [];
+  }
+}
+
 module.exports = {
-  syntax: 'postcss-scss',
-};
+  plugins: {
+    tailwindcss: './tailwind.js',
+    autoprefixer: {},
+    '@fullhuman/postcss-purgecss': process.env.NODE_ENV === 'production' ? {
+      content: [
+        './**/*.html',
+        '.*.html',
+        './assets/js/*.js',
+        './assets/styles/**/*.scss',
+
+      ],
+      extractors: [
+        {
+          extensions: ['html', 'scss', 'js'],
+          extractor: TailwindExtractor,
+        },
+      ],
+    } : false,
+  }
+}
